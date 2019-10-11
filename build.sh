5a54f174ee966b8620ecabc1bb2a14d3b5dd78a5
Chris Ruettimann <chris@bitbull.ch>
2019-10-11 10:10:15 +0200

    upgraded tags

--- build.sh
+++ build.sh
@@ -6,4 +6,6 @@ IMAGE=zabbix-server-mysql
 cd $(dirname $0) 
 
 docker build -t $IMAGE:$VERSION .
+docker tag $IMAGE:$VERSION christian773/zabbix-server-mysql:alpine-latest
+docker push christian773/zabbix-server-mysql:alpine-latest
 
